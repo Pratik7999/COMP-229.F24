a8ea645522e23c00b5d03f33c07f0f74c8c31f4f
Pratik7999 <156553276+Pratik7999@users.noreply.github.com>
2024-09-23 19:09:53 -0400

    week-2

--- app-02-vite/src/main.jsx
+++ app-02-vite/src/main.jsx
@@ -3,8 +3,15 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+const items = [
+  "1 cup of water",
+  "2 cup of water"
+];
 createRoot(document.getElementById('root')).render(
+
+
   <StrictMode>
-    <App />
+    <App item = {item}/>
   </StrictMode>,
 )
